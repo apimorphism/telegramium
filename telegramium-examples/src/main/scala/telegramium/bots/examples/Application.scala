39fdf778be47b87819db26181c92fd2d2c22bdca
johnspade <ivan@ilopatin.ru>
2020-04-21 01:33:17 +0300

    Add an example to show how inline keyboards work

--- telegramium-examples/src/main/scala/telegramium/bots/examples/Application.scala
+++ telegramium-examples/src/main/scala/telegramium/bots/examples/Application.scala
@@ -4,7 +4,7 @@ import telegramium.bots.client.ApiHttp4sImp
 
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
-
+import org.http4s.client.Client
 import org.http4s.client.middleware.Logger
 import org.http4s.client.blaze.BlazeClientBuilder
 
@@ -12,14 +12,26 @@ import scala.concurrent.duration.Duration
 
 object Application extends App {
 
-  val botApiToken = "BOT_API_TOKEN_GOT_FROM_BOTFATHER"
-  val baseUrl = s"https://api.telegram.org/bot$botApiToken"
-
   BlazeClientBuilder[Task](global).resource.use { httpClient =>
     val http = Logger(logBody = true, logHeaders = true)(httpClient)
-    val bot = new ApiHttp4sImp(http, baseUrl = baseUrl)
-    val echoBot = new EchoBot(bot)
-    echoBot.start()
+    args match {
+      case Array("EchoBot", token) =>
+        val echoBot = new EchoBot(createBotBackend(http, token))
+        echoBot.start()
+      case Array("IceCreamParlorBot", token) =>
+        val iceCreamParlorBot = new IceCreamParlorBot(createBotBackend(http, token))
+        iceCreamParlorBot.start()
+      case Array(name, _) =>
+        Task.raiseError(new RuntimeException(s"Unknown bot $name"))
+      case _ =>
+        Task.raiseError(new RuntimeException("Usage:\nApplication $botName $token"))
+    }
   }.runSyncUnsafe(Duration.Inf)
+
+  /**
+   * @param token Bot API token got from Botfather
+   */
+  private def createBotBackend(http: Client[Task], token: String) =
+    new ApiHttp4sImp(http, baseUrl = s"https://api.telegram.org/bot$token")
 
 }
