f192982324930e3327435398cd3bd3f7a540e9a7
johnspade <ivan@ilopatin.ru>
2020-06-28 14:42:26 +0300

    Remove http4s dependencies from telegramium-core

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,49 +3,62 @@ import sbt._
 object Dependencies {
 
   object V {
-    val monix      = "3.1.0"
-    val catsCore   = "2.1.1"
+    val monix = "3.1.0"
+    val catsCore = "2.1.1"
     val catsEffect = "2.1.3"
-    val circe      = "0.13.0"
-    val http4s     = "0.21.3"
-    val slf4j      = "1.7.26"
-    val logback    = "1.2.3"
-    val uPickle    = "0.8.0"
+    val circe = "0.13.0"
+    val http4s = "0.21.3"
+    val slf4j = "1.7.26"
+    val logback = "1.2.3"
+    val uPickle = "0.8.0"
+    val scalatest = "3.2.0"
+    val testcontainers = "0.37.0"
+    val mockServerClient = "5.10.0"
+    val scalamock = "4.4.0"
   }
 
-  val monix      = "io.monix" %% "monix" % V.monix
-  val catsCore   = "org.typelevel" %% "cats-core" % V.catsCore
+  val monix = "io.monix" %% "monix" % V.monix
+  val catsCore = "org.typelevel" %% "cats-core" % V.catsCore
   val catsEffect = "org.typelevel" %% "cats-effect" % V.catsEffect
+  val scalatest = "org.scalatest" %% "scalatest" % V.scalatest % Test
+  val scalamock = "org.scalamock" %% "scalamock" % V.scalamock % Test
 
   val circe = Seq(
     "io.circe" %% "circe-core"           % V.circe,
-    "io.circe" %% "circe-parser"         % V.circe,
+    "io.circe" %% "circe-parser"         % V.circe
   )
 
-  val http4s = Seq(
-    "org.http4s" %% "http4s-dsl"          % V.http4s,
+  val http4sClient = Seq(
     "org.http4s" %% "http4s-circe"        % V.http4s,
-    "org.http4s" %% "http4s-blaze-server" % V.http4s,
-    "org.http4s" %% "http4s-blaze-client" % V.http4s,
+    "org.http4s" %% "http4s-blaze-client" % V.http4s
+  )
+
+  val http4sServer = Seq(
+    "org.http4s" %% "http4s-dsl"          % V.http4s,
+    "org.http4s" %% "http4s-blaze-server" % V.http4s
   )
 
   val logger = Seq(
     "org.slf4j"      % "slf4j-api"       % V.slf4j,
     "org.slf4j"      % "slf4j-simple"    % V.slf4j,
-    "ch.qos.logback" % "logback-classic" % V.logback,
+    "ch.qos.logback" % "logback-classic" % V.logback
   )
 
   val uPickle = Seq(
     "com.lihaoyi" %% "upickle" % V.uPickle,
-    "com.lihaoyi" %% "upack" % V.uPickle,
+    "com.lihaoyi" %% "upack" % V.uPickle
   )
 
-  val common = Seq(
-    catsCore,
-    catsEffect,
-  ) ++ circe ++ http4s ++ uPickle
+  val testcontainers = Seq(
+    "com.dimafeng" %% "testcontainers-scala-scalatest" % V.testcontainers % Test,
+    "com.dimafeng" %% "testcontainers-scala-mockserver" % V.testcontainers % Test,
+    "org.mock-server" % "mockserver-client-java" % V.mockServerClient % Test
+  )
+
+  val common: Seq[ModuleID] = Seq(catsCore) ++ circe ++ uPickle
 
   val telegramiumCore: Seq[ModuleID] = common
-  val telegramiumHigh: Seq[ModuleID] = common
-  val telegramiumExam: Seq[ModuleID] = common ++ logger ++ Seq(monix)
+  val telegramiumHigh: Seq[ModuleID] = common ++ Seq(catsEffect, monix % Test, scalatest, scalamock) ++ http4sServer ++
+    http4sClient ++ testcontainers ++ logger
+  val telegramiumExam: Seq[ModuleID] = common ++ logger ++ Seq(catsEffect, monix) ++ http4sClient
 }
