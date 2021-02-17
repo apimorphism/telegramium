8f16238c4338af5a8cb706d2a48cb1d34870d168
Scala Steward <me@scala-steward.org>
2021-02-17 12:12:01 +0100

    Update cats-effect to 2.3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object V {
     val monix = "3.3.0"
     val catsCore = "2.4.1"
-    val catsEffect = "2.3.1"
+    val catsEffect = "2.3.2"
     val circe = "0.13.0"
     val http4s = "0.21.19"
     val slf4j = "1.7.30"
