ed113a0be93eff66f83bc27fcb9ecb6d6149c124
Enrico Benini <benkio89@gmail.com>
2021-06-29 15:52:15 +0100

    finally managed to run scalafmt on project folder using CLI

--- project/Compiler.scala
+++ project/Compiler.scala
@@ -15,12 +15,12 @@ object Compiler {
 
   val options = Seq(
     "-Xlint",
-    "-deprecation",                  // Emit warning and location for usages of deprecated APIs.
+    "-deprecation", // Emit warning and location for usages of deprecated APIs.
     "-encoding",
-    "utf-8",                         // Specify character encoding used by source files.
-    "-explaintypes",                 // Explain type errors in more detail.
-    "-feature",                      // Emit warning and location for usages of features that should be imported explicitly.
-    "-language:existentials",        // Existential types (besides wildcard types) can be written and inferred
+    "utf-8",                  // Specify character encoding used by source files.
+    "-explaintypes",          // Explain type errors in more detail.
+    "-feature",               // Emit warning and location for usages of features that should be imported explicitly.
+    "-language:existentials", // Existential types (besides wildcard types) can be written and inferred
     "-language:experimental.macros", // Allow macro definition (besides implementation and application)
     "-language:higherKinds",         // Allow higher-kinded types
     "-language:implicitConversions", // Allow definition of implicit functions called views
