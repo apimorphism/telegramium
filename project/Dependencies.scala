a662d82d54a89761d080da3693ae144d5d5a4fe8
Scala Steward <me@scala-steward.org>
2021-07-20 18:07:22 +0200

    Update slf4j-api, slf4j-simple to 1.7.32

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val catsEffect       = "2.5.1"
     val circe            = "0.14.1"
     val http4s           = "0.21.25"
-    val slf4j            = "1.7.31"
+    val slf4j            = "1.7.32"
     val logback          = "1.2.4"
     val uPickle          = "0.8.0"
     val scalatest        = "3.2.9"
