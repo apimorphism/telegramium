1470d41dc3459c01a7d0d5d710f20f74c048e88f
johnspade <ivan@ilopatin.ru>
2021-09-05 17:11:49 +0300

    Update Cats Effect to 3.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object V {
     val monix            = "3.4.0"
     val catsCore         = "2.6.1"
-    val catsEffect       = "3.2.5"
+    val catsEffect       = "3.2.7"
     val circe            = "0.14.1"
     val http4s           = "0.23.3"
     val slf4j            = "1.7.32"
