ec7ceb384482ab3ceee88d555c51f5e531d13fbd
Scala Steward <me@scala-steward.org>
2021-12-14 20:09:34 +0100

    Update logback-classic to 1.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val circe            = "0.14.1"
     val http4s           = "0.23.6"
     val slf4j            = "1.7.32"
-    val logback          = "1.2.7"
+    val logback          = "1.2.8"
     val scalatest        = "3.2.10"
     val testcontainers   = "0.39.12"
     val mockServerClient = "5.11.2"
