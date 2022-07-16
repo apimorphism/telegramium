801e9b5723c88d0eeecc6e8ea46f684b13e2d212
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-16 12:13:50 +0200

    Update log4cats-slf4j to 2.4.0 (#309)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val testcontainers   = "0.40.9"
     val mockServerClient = "5.11.2"
     val scalamock        = "5.1.0"
-    val log4cats         = "2.3.2"
+    val log4cats         = "2.4.0"
   }
 
   val catsCore   = "org.typelevel" %% "cats-core"   % V.catsCore
