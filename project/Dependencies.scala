ca057104d41c78d535bcff604c23e131eaa7cbb4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-22 18:27:36 +0200

    Update slf4j-api, slf4j-simple to 2.0.0 (#317)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val circe            = "0.14.2"
     val http4s           = "0.23.14"
     val blazeHttp4s      = "0.23.12"
-    val slf4j            = "1.7.36"
+    val slf4j            = "2.0.0"
     val logback          = "1.2.11"
     val scalatest        = "3.2.13"
     val testcontainers   = "0.40.10"
