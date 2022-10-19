79abf9a8614c92c09628150c36128fbf503d36f0
Scala Steward <scala_steward@virtuslab.com>
2022-10-19 13:48:32 +0000

    Revert commit(s) 00eb2d7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val blazeHttp4s      = "0.23.12"
     val slf4j            = "2.0.1"
     val logback          = "1.2.11"
-    val scalatest        = "3.2.14"
+    val scalatest        = "3.2.13"
     val testcontainers   = "0.40.10"
     val mockServerClient = "5.11.2"
     val log4cats         = "2.5.0"
