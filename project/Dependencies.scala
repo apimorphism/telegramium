5c08ba29ee027b3cd67ddb7774debf32c43f7e9f
Scala Steward <scala_steward@virtuslab.com>
2023-01-07 14:38:55 +0000

    Update scalatest to 3.2.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val blazeHttp4s      = "0.23.12"
     val slf4j            = "2.0.6"
     val logback          = "1.2.11"
-    val scalatest        = "3.2.14"
+    val scalatest        = "3.2.15"
     val testcontainers   = "0.40.12"
     val mockServerClient = "5.11.2"
     val log4cats         = "2.5.0"
