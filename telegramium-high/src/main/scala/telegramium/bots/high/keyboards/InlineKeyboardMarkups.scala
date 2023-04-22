616118fcd5c55762c41b6e78d550979bb5069527
ilopatin <ivan@ilopatin.ru>
2023-04-23 00:56:33 +0200

    Add varargs methods to InlineKeyboardMarkups

--- telegramium-high/src/main/scala/telegramium/bots/high/keyboards/InlineKeyboardMarkups.scala
+++ telegramium-high/src/main/scala/telegramium/bots/high/keyboards/InlineKeyboardMarkups.scala
@@ -12,7 +12,18 @@ object InlineKeyboardMarkups {
     */
   def singleRow(row: List[InlineKeyboardButton]): InlineKeyboardMarkup = InlineKeyboardMarkup(List(row))
 
+  /** Creates an inline keyboard markup with multiple buttons on a single row
+    */
+  def singleRow(firstButton: InlineKeyboardButton, buttons: InlineKeyboardButton*): InlineKeyboardMarkup =
+    InlineKeyboardMarkup(List(firstButton :: buttons.toList))
+
   /** Creates an inline keyboard markup with multiple buttons on a single column
     */
   def singleColumn(column: List[InlineKeyboardButton]): InlineKeyboardMarkup = InlineKeyboardMarkup(column.map(List(_)))
+
+  /** Creates an inline keyboard markup with multiple buttons on a single column
+    */
+  def singleColumn(firstButton: InlineKeyboardButton, buttons: InlineKeyboardButton*): InlineKeyboardMarkup =
+    InlineKeyboardMarkup((firstButton :: buttons.toList).map(List(_)))
+
 }
---
+++ telegramium-high/src/test/scala/telegramium/bots/high/keyboards/InlineKeyboardMarkupsSpec.scala
@@ -0,0 +1,46 @@
+package telegramium.bots.high.keyboards
+
+import org.scalatest.funsuite.AnyFunSuite
+import telegramium.bots.{InlineKeyboardButton, InlineKeyboardMarkup}
+
+class InlineKeyboardMarkupsSpec extends AnyFunSuite {
+
+  test("singleButton should create an inline keyboard markup with one button") {
+    val button = InlineKeyboardButtons.url("Button", "https://example.com")
+    val markup = InlineKeyboardMarkups.singleButton(button)
+    assert(markup == InlineKeyboardMarkup(List(List(button))))
+  }
+
+  test("singleRow should create an inline keyboard markup with multiple buttons on a single row") {
+    val row = List(
+      InlineKeyboardButtons.url("Button 1", "https://example.com"),
+      InlineKeyboardButtons.url("Button 2", "https://example.com")
+    )
+    val markup = InlineKeyboardMarkups.singleRow(row)
+    assert(markup == InlineKeyboardMarkup(List(row)))
+  }
+
+  test("singleRow (varargs) should create an inline keyboard markup with multiple buttons on a single row") {
+    val button1 = InlineKeyboardButtons.url("Button 1", "https://example.com")
+    val button2 = InlineKeyboardButtons.url("Button 2", "https://example.com")
+    val markup  = InlineKeyboardMarkups.singleRow(button1, button2)
+    assert(markup == InlineKeyboardMarkup(List(List(button1, button2))))
+  }
+
+  test("singleColumn should create an inline keyboard markup with multiple buttons on a single column") {
+    val column = List(
+      InlineKeyboardButtons.url("Button 1", "https://example.com"),
+      InlineKeyboardButtons.url("Button 2", "https://example.com")
+    )
+    val markup = InlineKeyboardMarkups.singleColumn(column)
+    assert(markup == InlineKeyboardMarkup(List(List(column.head), List(column.tail.head))))
+  }
+
+  test("singleColumn (varargs) should create an inline keyboard markup with multiple buttons on a single column") {
+    val button1 = InlineKeyboardButtons.url("Button 1", "https://example.com")
+    val button2 = InlineKeyboardButtons.url("Button 2", "https://example.com")
+    val markup  = InlineKeyboardMarkups.singleColumn(button1, button2)
+    assert(markup == InlineKeyboardMarkup(List(List(button1), List(button2))))
+  }
+
+}
