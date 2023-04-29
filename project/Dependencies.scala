585c795f4def16b6ad1b238e2a983ab8c120197d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-04-29 23:24:25 +0200

    Update cats-effect to 3.4.10 (#392)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object V {
     val catsCore         = "2.9.0"
-    val catsEffect       = "3.4.9"
+    val catsEffect       = "3.4.10"
     val circe            = "0.14.5"
     val http4s           = "0.23.18"
     val blazeHttp4s      = "0.23.14"
