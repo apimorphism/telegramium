8eb6e1d0d432bf19b383f8372a2332599246c695
Scala Steward <scala_steward@virtuslab.com>
2023-08-15 13:01:37 +0000

    Update cats-core to 2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object V {
-    val catsCore         = "2.9.0"
+    val catsCore         = "2.10.0"
     val catsEffect       = "3.5.1"
     val circe            = "0.14.5"
     val http4s           = "0.23.23"
