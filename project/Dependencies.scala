04eba4ca0121e762a1e72215d9c3fb9c1bbbfd39
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-01-30 15:40:13 +0100

    Update http4s-circe, http4s-dsl to 0.23.25 (#457)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val catsCore         = "2.10.0"
     val catsEffect       = "3.5.3"
     val circe            = "0.14.6"
-    val http4s           = "0.23.24"
+    val http4s           = "0.23.25"
     val blazeHttp4s      = "0.23.16"
     val slf4j            = "2.0.7"
     val logback          = "1.3.7"
