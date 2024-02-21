0dfffba8796d4f6ad95f233c4e240a7c353bc51a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-02-21 23:28:30 +0100

    Update scalatest to 3.2.18 (#463)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val blazeHttp4s      = "0.23.16"
     val slf4j            = "2.0.7"
     val logback          = "1.3.7"
-    val scalatest        = "3.2.17"
+    val scalatest        = "3.2.18"
     val testcontainers   = "0.41.3"
     val mockServerClient = "5.14.0"
   }
